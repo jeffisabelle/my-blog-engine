e08ef491673e327ae09adde5b246a7ce61976b9d
jeffisabelle <muhitosan@gmail.com>
2013-04-30 11:13:25 +0300

    removed stars from bottom

--- projects/views.py
+++ projects/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render_to_response
 
 def projects(request):
     all_projects = Project.objects.all()
-    print all_projects
+    last_project = all_projects[len(all_projects) - 1]
 
     return render_to_response('projects/projects.html',
-                              {'projects': all_projects})
+                              {'projects': all_projects,
+                               'last_project': last_project})
